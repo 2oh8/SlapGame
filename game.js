6bf798711e8a02d66baabe82f3528d5627e2fdfc
Matt Young <matthewdyoung@att.net>
2017-07-31 09:34:45 -0600

    Functional changes added.

--- game.js
+++ game.js
@@ -1,17 +1,34 @@
 
 var health = 100
+var hits = 0
 
-
-function attack(attackdamage){
-health -= attackdamage;
-console.log(health);
+function attack(attackdamage) {
+    health -= attackdamage;
+    console.log(health);
+    hits ++
+    healthBar(health)
 }
 
-function healthBar( input ){
-    document.getElementById("health").innerHTML = `
-    <div class="progress">
-        <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="100" aria-valuemin="0"
-        aria-valuemax="100" style="width: ${ input }%">`
+function healthBar(input) {
+    document.getElementById("stats").innerHTML = `
+    <div class="row">
+            <div class=" col-xs-12 attackpanel" role="group" aria-label="...">
+                <div class="attackbuttons">
+                    <div id=><p>Health: ${health} Hits: ${hits}</p></div>
+                    <span><span >
+                    <div id="health" class="progress">
+                        <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="100" aria-valuemin="0"
+                            aria-valuemax="100" style="width: ${health}%">
+                    </span>
+                </div>
+            </div>
+            <button type="button" onclick="attack(1)" class="btn btn-default attackbuttons"><i class="fa fa-bolt" aria-hidden="true"></i> Zapper</button>
+            <button type="button" onclick="attack(5)" class="btn btn-default attackbuttons"><i class="fa fa-lightbulb-o" aria-hidden="true"></i> Flashlight</button>
+            <button type="button" onclick="attack(10)" class="btn btn-default attackbuttons"><i class="fa fa-flask" aria-hidden="true"></i> Anti-ghost Potion</button>
+        </div>
+    </div>
+    </div>`
 }
 
+healthBar()
 
