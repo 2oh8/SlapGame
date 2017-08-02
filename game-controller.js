c2e3cad98be31447d54ad3499410740c41d94c7c
Matt Young <matthewdyoung@att.net>
2017-08-02 11:42:27 -0600

    Fully encapsulated, without mods.

--- game-controller.js
+++ game-controller.js
@@ -1,4 +1,62 @@
 
 function GameController(){
 
+//PRIVATE
+
+var gameService = new GameService()
+
+function draw() {
+    
+    var ghosts = gameService.getTarget()
+    var display = ''
+display += `<div id="gamerender" class="container-fluid">
+        <div class="row">
+            <div class="col-xs-12 main-game-content">
+                 <h>Ghostland</h>
+                <img id="ghostman" class="center-block" src="${ghosts.sprite}"></img>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class=" col-xs-12 attackpanel" role="group" aria-label="...">
+                <div class="attackbuttons">
+                    <div id=>
+                        <p>Health: ${ghosts.health} Hits: ${ghosts.hits}</p>
+                    </div>
+                    <span><span >
+                    <div id="health" class="progress">
+                        <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="100" aria-valuemin="0"
+                            aria-valuemax="100" style="width: ${ghosts.health}%">
+                    </span>
+                </div>
+            </div>
+            <button type="button" onclick="app.controllers.gameController.flash()" class="btn btn-default attackbuttons"><i class="fa fa-lightbulb-o" aria-hidden="true"></i> Flashlight</button>
+            <button type="button" onclick="app.controllers.gameController.zap()" class="btn btn-default attackbuttons"><i class="fa fa-bolt" aria-hidden="true"></i> Zapper</button>
+            <button type="button" onclick="app.controllers.gameController.magic()" class="btn btn-default attackbuttons"><i class="fa fa-magic" aria-hidden="true"></i> Magic Wand</button> 
+        </div>
+    </div>
+    </div>
+    </div>`
+    {
+    document.getElementById('gamerender').innerHTML = display
+    }
+}
+
+draw()
+
+//PUBLIC
+
+this.flash = function flashAttack(){
+ gameService.attack('flash')
+ draw()
+}
+this.zap = function zapAttack(){
+ gameService.attack('zap')
+ draw()
+}
+this.magic = function magicAttack(){
+ gameService.attack('magic')
+ draw()
+}
 }
--- game-service.js
+++ game-service.js
@@ -1,4 +1,36 @@
 
 function GameService(){
-    
+
+//PRIVATE
+
+function GhostMan(name, health, sprite, hits){
+    var ghostman = 
+        this.name = name; 
+        this.health = health; 
+        this.sprite = sprite; 
+        this.hits = hits;
+}
+
+var ghost1 = new GhostMan ('Ghostman', 100, "http://i.imgur.com/GKSfYIL.png",0)
+
+this.attack = function attack( input ){
+    if (ghost1.health > 0 && input === 'flash'){
+        ghost1.health -= 1
+        console.log(ghost1.health)
+    } else if (ghost1.health > 0 && input === 'zap'){
+        ghost1.health -= 5
+    } else if (ghost1.health > 0 && input === 'magic'){
+        ghost1.health -= 10
+    } else if (ghost1.health <= 0){
+        alert('Ghost has been eliminated')
+    }
+}
+
+//PUBLIC
+
+ this.getTarget = function getTarget(){
+    var ghostCopy = JSON.parse(JSON.stringify(ghost1))
+    return ghostCopy
+  }
+
 }
