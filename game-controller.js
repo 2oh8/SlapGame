294ab1a384a0d5bcb9a807494447ef9f3f658283
Matt Young <matthewdyoung@att.net>
2017-08-03 01:34:30 -0600

    Final changes prior to submission for grading.

--- game-controller.js
+++ game-controller.js
@@ -35,6 +35,97 @@ display += `<div id="gamerender" class="container-fluid">
             <button type="button" onclick="app.controllers.gameController.zap()" class="btn btn-default attackbuttons"><i class="fa fa-bolt" aria-hidden="true"></i> Zapper</button>
             <button type="button" onclick="app.controllers.gameController.magic()" class="btn btn-default attackbuttons"><i class="fa fa-magic" aria-hidden="true"></i> Magic Wand</button> 
         </div>
+        <div>
+            <button type="button" onclick="app.controllers.gameController.healthRecharge()" class="btn btn-default attackbuttons"><i class="fa fa-medkit" aria-hidden="true"></i> Health Recharge</button>
+            <button type="button" onclick="app.controllers.gameController.equipMegaBattery()" class="btn btn-default attackbuttons"><i class="fa fa-battery-full" aria-hidden="true"></i> Mega Battery</button>
+            <button type="button" onclick="app.controllers.gameController.equipPotion()" class="btn btn-default attackbuttons"><i class="fa fa-flask" aria-hidden="true"></i> Anit Ghost Potion</button> 
+        </div>
+    </div>
+    </div>
+    </div>`
+    {
+    document.getElementById('gamerender').innerHTML = display
+    }
+}
+
+function drawModsMegaBattery() {
+    
+    var ghosts = gameService.getTarget()
+    var display = ''
+display += `<div id="gamerender" class="container-fluid">
+        <div class="row">
+            <div class="col-xs-12 main-game-content">
+                 <h>Ghostland</h>
+                <img id="ghostman" class="center-block" src="${ghosts.sprite}"></img>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class=" col-xs-12 attackpanel" role="group" aria-label="...">
+                <div class="attackbuttons">
+                    <div id=>
+                        <p>Health: ${ghosts.health} Hits: ${ghosts.hits}</p>
+                    </div>
+                    <span><span >
+                    <div id="health" class="progress">
+                        <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="100" aria-valuemin="0"
+                            aria-valuemax="100" style="width: ${ghosts.health}%">
+                    </span>
+                </div>
+            </div>
+            <button type="button" onclick="app.controllers.gameController.megaFlash()" class="btn btn-default attackbuttons"><i class="fa fa-lightbulb-o" aria-hidden="true"></i> Flashlight</button>
+            <button type="button" onclick="app.controllers.gameController.megaZap()" class="btn btn-default attackbuttons"><i class="fa fa-bolt" aria-hidden="true"></i> Zapper</button>
+            <button type="button" onclick="app.controllers.gameController.magic()" class="btn btn-default attackbuttons"><i class="fa fa-magic" aria-hidden="true"></i> Magic Wand</button> 
+        </div>
+        <div>
+            <button type="button" onclick="app.controllers.gameController.healthRecharge()" class="btn btn-default attackbuttons"><i class="fa fa-medkit" aria-hidden="true"></i> Health Recharge</button>
+            <button type="button" onclick="" class="btn btn-default attackbuttons"><i class="fa fa-battery-full" aria-hidden="true"></i> Mega Battery</button>
+            <button type="button" onclick="" class="btn btn-default attackbuttons"><i class="fa fa-flask" aria-hidden="true"></i> Anit Ghost Potion</button> 
+        </div>
+    </div>
+    </div>
+    </div>`
+    {
+    document.getElementById('gamerender').innerHTML = display
+    }
+}
+
+function drawModsEquipPotion() {
+    
+    var ghosts = gameService.getTarget()
+    var display = ''
+display += `<div id="gamerender" class="container-fluid">
+        <div class="row">
+            <div class="col-xs-12 main-game-content">
+                 <h>Ghostland</h>
+                <img id="ghostman" class="center-block" src="${ghosts.sprite}"></img>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class=" col-xs-12 attackpanel" role="group" aria-label="...">
+                <div class="attackbuttons">
+                    <div id=>
+                        <p>Health: ${ghosts.health} Hits: ${ghosts.hits}</p>
+                    </div>
+                    <span><span >
+                    <div id="health" class="progress">
+                        <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="100" aria-valuemin="0"
+                            aria-valuemax="100" style="width: ${ghosts.health}%">
+                    </span>
+                </div>
+            </div>
+            <button type="button" onclick="app.controllers.gameController.flash()" class="btn btn-default attackbuttons"><i class="fa fa-lightbulb-o" aria-hidden="true"></i> Flashlight</button>
+            <button type="button" onclick="app.controllers.gameController.zap()" class="btn btn-default attackbuttons"><i class="fa fa-bolt" aria-hidden="true"></i> Zapper</button>
+            <button type="button" onclick="app.controllers.gameController.megaWand()" class="btn btn-default attackbuttons"><i class="fa fa-magic" aria-hidden="true"></i> Magic Wand</button> 
+        </div>
+        <div>
+            <button type="button" onclick="app.controllers.gameController.healthRecharge()" class="btn btn-default attackbuttons"><i class="fa fa-medkit" aria-hidden="true"></i> Health Recharge</button>
+            <button type="button" onclick="" class="btn btn-default attackbuttons"><i class="fa fa-battery-full" aria-hidden="true"></i> Mega Battery</button>
+            <button type="button" onclick="" class="btn btn-default attackbuttons"><i class="fa fa-flask" aria-hidden="true"></i> Anit Ghost Potion</button> 
+        </div>
     </div>
     </div>
     </div>`
@@ -58,5 +149,29 @@ this.zap = function zapAttack(){
 this.magic = function magicAttack(){
  gameService.attack('magic')
  draw()
+}
+this.healthRecharge = function healthRecharge(){
+gameService.attack('health')
+    draw()
+}
+this.equipMegaBattery = function megaFlash(){
+    drawModsMegaBattery()
+    
+}
+this.equipPotion = function megaFlash(){
+    drawModsEquipPotion()
+
+}
+this.megaFlash = function megaFlash(){
+    gameService.attack('megaFlash')
+    drawModsMegaBattery()
+}
+this.megaZap = function megaZap(){
+    gameService.attack('megaZap')
+    drawModsMegaBattery()
+}
+this.megaWand = function megaWand(){
+    gameService.attack('ghostPotionActive')
+    drawModsEquipPotion()
 }
 }
--- game-service.js
+++ game-service.js
@@ -1,36 +1,49 @@
 
-function GameService(){
+function GameService() {
 
-//PRIVATE
+    //PRIVATE
 
-function GhostMan(name, health, sprite, hits){
-    var ghostman = 
-        this.name = name; 
-        this.health = health; 
-        this.sprite = sprite; 
+    function GhostMan(name, health, sprite, hits) {
+        var ghostman =
+            this.name = name;
+        this.health = health;
+        this.sprite = sprite;
         this.hits = hits;
-}
-
-var ghost1 = new GhostMan ('Ghostman', 100, "http://i.imgur.com/GKSfYIL.png",0)
-
-this.attack = function attack( input ){
-    if (ghost1.health > 0 && input === 'flash'){
-        ghost1.health -= 1
-        console.log(ghost1.health)
-    } else if (ghost1.health > 0 && input === 'zap'){
-        ghost1.health -= 5
-    } else if (ghost1.health > 0 && input === 'magic'){
-        ghost1.health -= 10
-    } else if (ghost1.health <= 0){
-        alert('Ghost has been eliminated')
     }
-}
 
-//PUBLIC
+    var ghost1 = new GhostMan('Ghostman', 100, "http://i.imgur.com/GKSfYIL.png", 0)
 
- this.getTarget = function getTarget(){
-    var ghostCopy = JSON.parse(JSON.stringify(ghost1))
-    return ghostCopy
-  }
+    this.attack = function attack(input) {
+        if (ghost1.health > 0 && input === 'flash') {
+            ghost1.health -= 1
+            ghost1.hits++
+        } if (ghost1.health > 0 && input === 'zap') {
+            ghost1.health -= 5
+            ghost1.hits++
+        } if (ghost1.health > 0 && input === 'magic') {
+            ghost1.health -= 10
+            ghost1.hits++
+        } if (ghost1.health > 0 && input === 'megaFlash') {
+            ghost1.health -= 4
+            ghost1.hits ++
+        } if (ghost1.health > 0 && input === 'megaZap') {
+            ghost1.health -= 12
+            ghost1.hits ++ 
+        } if (ghost1.health > 0 && input === 'ghostPotionActive') {
+            ghost1.health -= 25
+            ghost1.hits ++ 
+        } if (ghost1.health <= 0) {
+            alert('Ghost has been eliminated')
+        } if (ghost1.health < 100 && input === 'health') {
+            ghost1.health = 100
+        } 
+    }
+
+    //PUBLIC
+
+    this.getTarget = function getTarget() {
+        var ghostCopy = JSON.parse(JSON.stringify(ghost1))
+        return ghostCopy
+    }
 
 }
